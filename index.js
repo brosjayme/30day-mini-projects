e056c0b73f460b6ad5dd3bcca693b5a757ad6dd2
unknown <brosjayme@gmail.com>
2023-04-21 07:51:25 +0100

    mini projects

---
+++ index.js
@@ -0,0 +1,9 @@
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "pre@scrimba.com",
+    "02": "frode@scrimba.com"
+}
+
+let scrimbaUsersEmails = Object.values(scrimbaUsers)
+
+console.log(scrimbaUsers)
