1ceebca278c012434880e79297b3efa0a88d76cd
unknown <brosjayme@gmail.com>
2023-09-08 10:28:29 +0100

    oop syntax

--- oop.js
+++ oop.js
@@ -52,17 +52,72 @@
 // const sam = new Elf('sam', 'fire')
 // sam.name
 
-//es6 Class
-class Elf {
+// //es6 Class
+// class Elf {
+//   constructor(name, weapon) {
+//     this.name = name;
+//     this.weapon = weapon;
+//   }
+// }
+// Elf.prototype.attack = function () {
+//   return "attack with" + this.weapon;
+// };
+
+// const peter = new Elf("peter", "stones");
+// const sam = new Elf("sam", "fire");
+// sam.name;
+
+// // This keyword
+// //new binding this
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// const Person = new Person("John", 55);
+// person1;
+
+// // implicit binding
+// const person = {
+//   name: "Karen",
+//   age: 40,
+//   hi() {
+//     console.log("hi" + this.name);
+//   },
+// };
+
+class Character {
   constructor(name, weapon) {
     this.name = name;
     this.weapon = weapon;
   }
 }
-Elf.prototype.attack = function () {
-  return "attack with" + this.weapon;
-};
 
-const peter = new Elf("peter", "stones");
-const sam = new Elf("sam", "fire");
-sam.name;
+class Elf extends Character {
+  constructor(name, weapon, type) {
+    super(name, weapon);
+    this.name = name;
+    this.weapon = weapon;
+    this.type = type;
+  }
+  attack() {
+    return "attack with" + this.weapon;
+  }
+}
+
+class Ogre extends Character {
+  constructor(name, weapon, color) {
+    super(name, weapon);
+    this.name = name;
+    this.weapon = weapon;
+    this.color = color;
+  }
+  makeTent() {
+    return "makes the best tents in world";
+  }
+}
+
+const fiona = new Elf("Fiona", "ninja stars", "house");
+const dolby = new Ogre("dolby", "cloth", "blue");
+fiona;
