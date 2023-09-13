ff22329ef43e6d35ed4ef2b220a1d326182fee60
unknown <brosjayme@gmail.com>
2023-09-13 09:02:55 +0100

    amazon excercise

--- amazon.js
+++ amazon.js
@@ -4,11 +4,11 @@ const user = {
   cart: [],
   purchases: [],
 };
-
-const compose =
-  (f, g) =>
-  (...args) =>
-    f(g(...args));
+const amazonHistory = [];
+const compose = (f, g) => {
+  console.log("compose: ", f, g);
+  return (...args) => f(g(...args));
+};
 purchaseItem(
   emptyCart,
   buyItem,
@@ -21,11 +21,13 @@ function purchaseItem(...fns) {
 }
 
 function addItemToCart(user, item) {
+  amazonHistory.push(user);
   const updateCart = user.cart.concat(item);
   return Object.assign({}, user, { cart: updateCart });
 }
 
 function applyTaxToItems(user) {
+  amazonHistory.push(user);
   const { cart } = user;
   const taxRate = 1.3;
   const updateCart = cart.map((item) => {
@@ -38,15 +40,18 @@ function applyTaxToItems(user) {
 }
 
 function buyItem(user) {
-  return user;
+  amazonHistory.push(user);
+  return Object.assign({}, user, { purchases: user.cart });
 }
 
 function emptyCart(user) {
-  return user;
+  amazonHistory.push(user);
+  return Object.assign({}, user, { cart: [] });
 }
 
 //implement a cart feature:
 //1. add items to cart.
-//2. add 3% tax to item in cart
+//2. add 30% tax to item in cart
 //3. buy item : cart --> purchases
 //4. empty cart
+// console.log(amazonHistory);
