2d94644cdfbaddcd3eacb71200bce396dfda58b3
unknown <brosjayme@gmail.com>
2023-09-14 07:31:38 +0100

    promise

--- promise.js
+++ promise.js
@@ -9,22 +9,43 @@
 //   });
 // });
 
-// best practice
-grabTweets("twitter/brosjayme", (error, brosjaymeTweets) => {
-  if (error) {
-    throwError;
+// // best practice
+// grabTweets("twitter/brosjayme", (error, brosjaymeTweets) => {
+//   if (error) {
+//     throwError;
+//   }
+//   displayTweets(brosjaymeTweets);
+//   grabTweets("twitter/elonmusk", (error, elonTweets) => {
+//     if (error) {
+//       throwError;
+//     }
+//     displayTweets(elonTweets);
+//     grabTweets("twitter/john", (error, johnTweets) => {
+//       if (error) {
+//         throwError;
+//       }
+//       displayTweets(johnTweets);
+//     });
+//   });
+// });
+
+//
+const promise = new Promise((resolve, reject) => {
+  if (true) {
+    resolve("stuff worked");
+  } else {
+    reject("error, it broke");
   }
-  displayTweets(brosjaymeTweets);
-  grabTweets("twitter/elonmusk", (error, elonTweets) => {
-    if (error) {
-      throwError;
-    }
-    displayTweets(elonTweets);
-    grabTweets("twitter/john", (error, johnTweets) => {
-      if (error) {
-        throwError;
-      }
-      displayTweets(johnTweets);
-    });
-  });
 });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "hii!!");
+});
+
+promise
+  .then((result) => result + "!")
+  .then((result2) => retult2 + "?")
+  .catch(() => console.log("error!"))
+  .then((result3) => {
+    console.log(result3 + "!");
+  });
