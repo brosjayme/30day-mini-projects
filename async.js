740c8d62fad292b7f7e8267969ec63e8b4b8f104
unknown <brosjayme@gmail.com>
2023-09-17 21:22:58 +0100

    first commit

--- async.js
+++ async.js
@@ -1,13 +1,5 @@
-setTimeout(() => {
-  console.log("1", "is the loneliest number");
-}, 0);
-
-setTimeout(() => {
-  console.log("2", "can be as bad as one");
-}, 10);
-
-//2
-Promise.resolve("hi").then((data) => console.log("2", data));
-
-//3
-console.log("3", "is a crowd");
+async function fetchUsers() {
+  const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await resp.json();
+  console.log(data);
+}
---
+++ es20.js
@@ -0,0 +1,6 @@
+const animals = {
+  tiger: 25,
+  lion: 16,
+  monkey: 4,
+};
+const { tiger, ...rest } = animals;
